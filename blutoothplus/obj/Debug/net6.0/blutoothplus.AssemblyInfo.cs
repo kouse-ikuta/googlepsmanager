e0d8e9ce85596a2fa3a9f334c3629a2fb8538298
kouse-ikuta <e1n21009@oit.ac.jp>
2024-09-20 18:20:56 +0900

    C#09/20

--- blutoothplus/obj/Debug/net6.0/blutoothplus.AssemblyInfo.cs
+++ blutoothplus/obj/Debug/net6.0/blutoothplus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blutoothplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71c2f7526d33b92dedb5785101b978e875cbc8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("blutoothplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blutoothplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
